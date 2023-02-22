f60208982d9fec76b7945f51adb2053cd72828dd
WernersGit <124093481+WernersGit@users.noreply.github.com>
2023-02-22 19:53:56 +0100

    First Try

--- domain/src/main/java/projekt/base/ChessboardDistanceCalculator.java
+++ domain/src/main/java/projekt/base/ChessboardDistanceCalculator.java
@@ -4,7 +4,7 @@ package projekt.base;
  * A calculator for the distance between two given points using a
  * chessboard as world grid.
  */
-public class ChessboardDistanceCalculator implements DistanceCalculator { //see if its working
+public class ChessboardDistanceCalculator implements DistanceCalculator { //see if its working?
     @Override
     public double calculateDistance(Location a, Location b) {
         return Math.max(
